25c0507886694358bc287645366caaaf52965ab7
mohammadreza rouhani <rezarouhanitonekaboni@gmail.com>
2023-09-17 23:44:03 +0330

    top header updatd

--- JewelryStoreFront/components/SearchInput.vue
+++ JewelryStoreFront/components/SearchInput.vue
@@ -2,8 +2,11 @@
   <div class="relative flex w-full flex-wrap items-stretch">
     <input
       type="search"
-      class="relative m-0 block flex-auto rounded border border-solid text-lg border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none text-white"
-      placeholder="Search"
+      class="relative m-0 block flex-auto rounded border border-solid text-lg border-neutral-300 
+      bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] 
+      text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary 
+      focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none text-white"
+      placeholder="جستجو"
       aria-label="Search"
       aria-describedby="button-addon2"
     />
